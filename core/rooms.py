14b296561e2111523f79639fd66dfeb93ee50a48
wattanaqpt-ai <wattanaqpt@gmail.com>
2026-05-18 23:57:58 +0700

    Update rooms.py

--- core/rooms.py
+++ core/rooms.py
@@ -1,1 +1,16 @@
 rooms = {}
+def create_room(chat_id):
+
+    rooms[chat_id] = {
+
+        "started": False,
+
+        "masters": [],
+        "players": [],
+
+        "master_turn": 0,
+
+        "active_mission": None,
+
+        "mission_lock": False
+    }
