eb0104a172dca975f6826a058ec59dba36013ab6
wattanaqpt-ai <wattanaqpt@gmail.com>
2026-05-19 23:09:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 from telegram.ext import (
     ApplicationBuilder,
-    CommandHandler
+    CommandHandler,
+    CallbackQueryHandler,
+    MessageHandler,
+    filters
 )
 
 from config import TOKEN
 
+# handlers
 from handlers.game import startgame
 from handlers.join import join
 from handlers.mission import mission
 
+from handlers.claim import claim_callback
+from handlers.media_handler import media_handler
+from handlers.vote import vote_callback
+
+# build app
 app = ApplicationBuilder().token(TOKEN).build()
 
+# commands
 app.add_handler(
     CommandHandler(
         "startgame",
@@ -32,6 +42,31 @@ app.add_handler(
     )
 )
 
+# claim button
+app.add_handler(
+    CallbackQueryHandler(
+        claim_callback,
+        pattern="claim"
+    )
+)
+
+# vote button
+app.add_handler(
+    CallbackQueryHandler(
+        vote_callback,
+        pattern="vote_"
+    )
+)
+
+# media handler
+app.add_handler(
+    MessageHandler(
+        filters.PHOTO | filters.VIDEO,
+        media_handler
+    )
+)
+
 print("BOT RUNNING...")
 
+# run bot
 app.run_polling()
