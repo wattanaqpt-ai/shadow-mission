7ee0b914d0ad91b58afa9946ab0c56b63ea12ab4
wattanaqpt-ai <wattanaqpt@gmail.com>
2026-05-20 12:57:26 +0700

    Add message_ids to room creation

--- core/rooms.py
+++ core/rooms.py
@@ -1,21 +1,14 @@
 rooms = {}
 
 def create_room(chat_id):
-
     rooms[chat_id] = {
-
         "started": False,
-
         "masters": [],
         "slaves": [],
-
         "master_turn": 0,
-
         "active_mission": None,
-
         "mission_lock": False,
-
         "scores": {},
-        
-        "endgame_votes": {"yes": [], "no": []}  # ← เพิ่ม
-}
+        "endgame_votes": {"yes": [], "no": []},
+        "message_ids": []
+    }
